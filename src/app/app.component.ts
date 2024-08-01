896e9edea361adf6da1e3a2e8b1bd66f69f73e75
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-08-01 18:22:20 +0530

    chore: Update footer component styles and structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./components/footer/footer.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,16 +6,14 @@ export const routes: Routes = [
   {
     path: 'register',
     loadComponent: () =>
-      import('./components/register/register.component').then(
+      import('./auth/register/register.component').then(
         (m) => m.RegisterComponent
       ),
   },
   {
     path: 'login',
     loadComponent: () =>
-      import('./components/login/login.component').then(
-        (m) => m.LoginComponent
-      ),
+      import('./auth/login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: 'tasks',
