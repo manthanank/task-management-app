e49ca8a3ee2fcfe7f54b3202e6bce2247f3e7f03
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-11-09 22:26:06 +0530

    feat: Implement pagination for user retrieval and include total user count in response

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,12 +2,19 @@ const User = require("../models/users.model");
 const { successResponse, errorResponse } = require("../utils/response");
 
 exports.getUsers = async (req, res) => {
-  try {
-    const users = await User.find().select("-password");
-    return successResponse(res, 200, "Users retrieved successfully", users);
-  } catch (error) {
-    return errorResponse(res, error.message);
-  }
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const users = await User.find().select("-password").skip(skip).limit(limit);
+  const totalUsers = await User.countDocuments();
+
+  return successResponse(res, 200, "Users retrieved successfully", {
+    users,
+    totalUsers,
+    totalPages: Math.ceil(totalUsers / limit),
+    currentPage: page,
+  });
 };
 
 exports.getUser = async (req, res) => {
